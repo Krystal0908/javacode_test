ea7bfae920e0a70d9d374e4f926e680b5aa4ad36
Krystal <978242273@qq.com>
2023-08-26 18:34:40 +0800

    test commit

---
+++ src/main/java/com/atguigu/javacode/JavacodeTestApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.javacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacodeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavacodeTestApplication.class, args);
+    }
+
+}
