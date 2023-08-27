7bc8a8a6d3c617245c720c1ab482bbf0c0e87baa
Krystal <978242273@qq.com>
2023-08-27 17:29:37 +0800

    test commit

---
+++ maven-pom-dependenies-02/src/main/java/com/atguigu/Main.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author krystal
+ * @create 2023-08-27 15:44
+ */
+public class Main {
+
+    public void tets2(){
+        
+    }
+}
---
+++ maven-pom-dependenies-02/src/test/java/com/atguigu/MavenTest.java
@@ -0,0 +1,15 @@
+package com.atguigu;
+
+import org.junit.Test;
+
+/**
+ * @author krystal
+ * @create 2023-08-27 15:43
+ */
+public class MavenTest {
+
+    @Test
+    public void test() {
+
+    }
+}
